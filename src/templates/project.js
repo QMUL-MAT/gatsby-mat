7cee758e1fb5b7daa180d4dec1e0d4de789303fe
Tom Gurion <nagasaki45@gmail.com>
2021-05-20 15:04:27 +0100

    Fix project page banner

--- src/templates/project.js
+++ src/templates/project.js
@@ -37,7 +37,7 @@ export const query = graphql`
 export default function Project({ data }) {
   const element = data.markdownRemark
   return (
-    <ContentPage pageTitle="Project" header="/images/students_header.jpg">
+    <ContentPage pageTitle="Project" header="/images/projects_header.jpg">
       <div class="columns">
         <div class="column is-one-third text-content">
           <h1>
